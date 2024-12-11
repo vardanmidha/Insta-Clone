71aad45f3d6165dd6611b507605a228233c81ab0
vardanmidha <anshmidha2@gmail.com>
2024-12-12 01:49:53 +0530

    First commit

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define vl vector<long long> 
+#define v1 vector<int> 
+#define v2 vector<vector<int>> 
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0) 
+using namespace std ;
+
+int main()
+{
+
+    
+    cout<<"Hello DOggy"<<endl;
+    return 0;
+
+}
